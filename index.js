a434e679146371aaebe0d149008c4885bcf8249b
Emmanuel Donate <emmanual.omar.d@gmail.com>
2017-05-26 22:47:01 -0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $.getJSON("topspots.json", function(data) { // pulling in the data
       $.each(data, function(index, item) {
         var currentRow = "<tr><td>" + item.name + "</td><td>" + item.description + "</td><td><a href='https://www.google.com/maps?q=" + item.location + "'><button id='submitButton' class='btn btn - primary'>Open in Maps</button></a></td></tr>";
-        $(currentRow).appendTo('#goHere');
+        $(currentRow).appendTo('#result');
       });
     });
   };
