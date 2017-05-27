88419495d585b60d68072be63fda8956df6c0e6c
Emmanuel Donate <emmanual.omar.d@gmail.com>
2017-05-26 23:05:03 -0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   listSpots();
   function listSpots() {
-    $.getJSON("topspots.json", function(data) { // pulling in the data
+    $.getJSON("topspots.json", function(data) {
+      // pulling in the data
       $.each(data, function(index, item) {
         var currentRow = "<tr><td>" + item.name + "</td><td>" + item.description + "</td><td><a href='https://www.google.com/maps?q=" + item.location + "'><button id='submitButton' class='btn btn - primary'>Open in Maps</button></a></td></tr>";
         $(currentRow).appendTo('#result');
